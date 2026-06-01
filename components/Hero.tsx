c384f2ca436c5537a8697abb5db37719d2601c82
Abdisamad Yusuf <58722262+abdisamadjoe@users.noreply.github.com>
2026-06-01 16:23:44 +0300

    chore: update hero image asset to misssomalipsd1.png

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -18,7 +18,7 @@ export default function Hero() {
       <div className="relative w-full z-20 flex flex-col flex-1">
         <div className="grid-container flex-1 flex flex-col justify-between">
           <div className="grid-12 items-center gap-y-12 lg:gap-y-0 flex-1 pt-12 pb-0 lg:py-0">
-            
+
             {/* Left Column: Content & Stats (Columns 1-5 on desktop, 12 on mobile) */}
             <div className="col-span-12 lg:col-span-5 flex flex-col items-center lg:items-start text-center lg:text-left">
               {/* Luxury Badge */}
@@ -35,7 +35,7 @@ export default function Hero() {
 
               {/* Luxury Divider */}
               <div className="w-16 h-[2px] bg-[#E8C97A] my-6 rounded-full" />
-              
+
               {/* Slogan */}
               <p className="text-[15px] font-light text-[#F5F0E8]/70 leading-[1.7] max-w-[420px] text-center lg:text-left">
                 Are you the woman who will wear the crown and represent Somali women on the world stage?
@@ -64,14 +64,14 @@ export default function Hero() {
 
             {/* Right Column: Portrait Image & Backdrops (Columns 6-12 on desktop, 12 on mobile) */}
             <div className="col-span-12 lg:col-span-7 flex justify-center items-end relative lg:h-[86vh] xl:h-[95vh] z-10 lg:self-end pb-0 mb-0">
-              
+
               {/* Glowing Gold Halo Spotlight */}
               <div className="absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2 w-[350px] h-[350px] sm:w-[450px] sm:h-[450px] lg:w-[600px] lg:h-[600px] bg-gradient-to-r from-[#E8C97A]/25 to-transparent blur-3xl rounded-full z-0 pointer-events-none hidden md:block" />
 
               {/* Model Image wrapper */}
               <div className="relative w-full h-[55vh] sm:h-[60vh] lg:h-full max-w-[380px] sm:max-w-[460px] lg:max-w-none mx-auto z-10 pb-0 mb-0">
                 <Image
-                  src="/images/misssomalipsd.png"
+                  src="/images/misssomalipsd1.png"
                   alt="Miss Somali - Coronation"
                   fill
                   priority
